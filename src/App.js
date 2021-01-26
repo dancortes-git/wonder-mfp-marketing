f2502d0e377a4d5dc20d36caa2167d1b4d0d9aaf
Daniel Cortes Cespedes <daniel@dancortes.com>
2021-01-26 20:50:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'mkt',
+});
+
+export default ({ history, isSignedIn }) => {
+
+  console.log('Rendering marketing...', 'isSignedIn:', isSignedIn);
+
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/pricing">
+            <Pricing />
+          </Route>
+          <Route path="/">
+            <Landing isSignedIn={isSignedIn} />
+          </Route>
+        </Switch>
+      </Router>
+    </StylesProvider>
+  );
+};
